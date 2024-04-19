66d78275ef9571bf43290b490fd0b83213a90dda
Hilal-Bilun-Kara <bilunkara@gmail.com>
2024-04-19 17:18:09 +0300

    Banner-Projects Map

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,6 @@
-
-import BannerFoto from "../assets/bannerfoto.svg"
+import BannerFoto from "../assets/bannerfoto.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub,faLinkedinIn} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import Ellipse from "../assets/Ellipse.svg";
 import { useTranslation } from 'react-i18next';
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
@@ -9,58 +8,61 @@ import DarkMode from "./DarkMode";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
-
+const socialMediaLinks = [
+  {
+    icon: faLinkedinIn,
+    link: "https://www.linkedin.com/in/hilal-bilun-kara/"
+  },
+  {
+    icon: faGithub,
+    link: "https://github.com/Hilal-Bilun-Kara"
+  }
+];
 
 export default function Banner() {
-    const { darkMode,setDarkMode } = useContext(UserContext);
+  const { darkMode, setDarkMode } = useContext(UserContext);
+  const { t, i18n } = useTranslation();
 
-    const { t, i18n } = useTranslation();
- 
-    const clickHandle = lng => {
-      if (lng === 'tr') {
-        i18n.changeLanguage('tr');
-      } else if (lng === 'en') {
-        i18n.changeLanguage('en');
-      }
+  const clickHandle = lng => {
+    if (lng === 'tr') {
+      i18n.changeLanguage('tr');
+    } else if (lng === 'en') {
+      i18n.changeLanguage('en');
     }
+  };
 
-    
-    
-    return(
-        <div  className={darkMode ? 'dark' : ''}>
-        <section className={`w-screen bg-[#F4F4F4] dark:bg-[#2A262B] h-screen' overflow-hidden relative px-0`}>
-            <DarkMode darkMode={darkMode} setDarkMode={setDarkMode}/>
-            <Link className="text-[#EA2678] ml-[150vh]" to={i18n.lng === '/en' ? '/tr' : '/en'} onClick={() => clickHandle(i18n.language === 'en' ? 'tr' : 'en')}>
-                {i18n.language === 'en' ? 'Türkçe\'ye Geç' : 'Switch to English'}</Link>
-            <div className="w-8/12 flex justify-between items-center mx-auto">
-                <div className="flex flex-col w-[610px] mt-[125px]">
-                    <span className="absolute border-[55px] border-[#D9D9D9]  rounded-full left-[471px] top-[-40px]"></span>
-                    <img className="absolute w-[121px] top-[620px] left-[1050px]"
-                    src= {BannerFoto}/>
-                    <div className="flex flex-col items-start text-start">
-                        <h2 className="text-[20px] text-[#000000] dark:text-[#FFFFFF]">{t('greeting')}</h2>
-                        <h1 className="text-[25px] text-[#0A0A14] font medium mt-3 relative z-50 dark:text-[#FFFFFF]">{t('mainText')}</h1>
-                    <span className="absolute rounded-md w-[108px] h-[18px] bg-[#E92577] left-[220px] top-[212px]"></span>
-                    </div>
-                    <footer className="flex flex-col items-start my-10">
-                        <div className="flex gap-5">
-                        <a href="https://www.linkedin.com/in/hilal-bilun-kara/" target="_blank" >
-                            <FontAwesomeIcon className="w-[30px] h-[33px] text-black dark:text-[#FFFFFF]" icon={faLinkedinIn}/>
-                            </a>
-
-                            <a href="https://github.com/Hilal-Bilun-Kara" target="_blank" >
-                                <FontAwesomeIcon className="w-[30px] h-[33px] text-black dark:text-[#FFFFFF]" icon={faGithub} />
-                                </a>                             
-                        </div>
-                        <p className="text-[15px] text-start w-9/12 mt-3 my-10 dark:text-[#FFFFFF]">
-                        {t('freelancingText')} <span className="text-[#AF0C48]">{t('freelancingText1')}</span> {t('freelancingText2')} <span className="text-[#AF0C48]">{t('freelancingText3')} </span>{t('freelancingText4')}<a className="text-[#AF0C48]">{t('email')}</a>
-                        </p>
-                    </footer>
-                </div>
-                <img className="absolute border-[20px] border-[#D9D9D9] dark:border-[#525252] border-transparent rounded-full left-[1000px] top-[380px]" src={Ellipse}/>
+  return (
+    <div className={darkMode ? 'dark' : ''}>
+      <section className={`w-screen bg-[#F4F4F4] dark:bg-[#2A262B] h-screen overflow-hidden relative px-0`}>
+        <DarkMode darkMode={darkMode} setDarkMode={setDarkMode}/>
+        <Link className="text-[#EA2678] ml-[150vh]" to={i18n.lng === '/en' ? '/tr' : '/en'} onClick={() => clickHandle(i18n.language === 'en' ? 'tr' : 'en')}>
+          {i18n.language === 'en' ? 'Türkçe\'ye Geç' : 'Switch to English'}
+        </Link>
+        <div className="w-8/12 flex justify-between items-center mx-auto">
+          <div className="flex flex-col w-[610px] mt-[125px]">
+            <span className="absolute border-[55px] border-[#D9D9D9]  rounded-full left-[471px] top-[-40px]"></span>
+            <img className="absolute w-[121px] top-[620px] left-[1050px]" src={BannerFoto}/>
+            <div className="flex flex-col items-start text-start">
+              <h2 className="text-[20px] text-[#000000] dark:text-[#FFFFFF]">{t('greeting')}</h2>
+              <h1 className="text-[25px] text-[#0A0A14] font medium mt-3 relative z-50 dark:text-[#FFFFFF]">{t('mainText')}</h1>
+              <span className="absolute rounded-md w-[108px] h-[18px] bg-[#E92577] left-[220px] top-[212px]"></span>
             </div>
-
-        </section>
+            <footer className="flex flex-col items-start my-10">
+              <div className="flex gap-5">
+                {socialMediaLinks.map((item, index) => (
+                  <a key={index} href={item.link} target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon className="w-[30px] h-[33px] text-black dark:text-[#FFFFFF]" icon={item.icon}/>
+                  </a>
+                ))}
+              </div>
+              <p className="text-[15px] text-start w-9/12 mt-3 my-10 dark:text-[#FFFFFF]">
+                {t('freelancingText')} <span className="text-[#AF0C48]">{t('freelancingText1')}</span> {t('freelancingText2')} <span className="text-[#AF0C48]">{t('freelancingText3')} </span>{t('freelancingText4')}<a className="text-[#AF0C48]">{t('email')}</a>
+              </p>
+            </footer>
+          </div>
+          <img className="absolute border-[20px] border-[#D9D9D9] dark:border-[#525252] border-transparent rounded-full left-[1000px] top-[380px]" src={Ellipse}/>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,63 +1,61 @@
 import React, { useContext } from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
-import RandomJokes from "../assets/RandomJokes.svg";
-import AreYouBored from "../assets/AreYouBored.svg";
 import { useTranslation } from 'react-i18next';
 import { UserContext } from '../context/UserContext';
 
+import RandomJokes from "../assets/RandomJokes.svg";
+import AreYouBored from "../assets/AreYouBored.svg";
+
 export default function Project() {
     const { darkMode } = useContext(UserContext);
-
     const { t } = useTranslation();
+
+    const projects = [
+        {
+            title: "Random Jokes",
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+            tags: ["react", "vercel", "axios", "router"],
+            githubLink: "https://github.com/Hilal-Bilun-Kara/redux-filmler-solution",
+            appLink: "https://fsweb-s10g2-redux-filmler-solution-3cag.onrender.com",
+            image: RandomJokes
+        },
+        {
+            title: "Are you bored?",
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia minima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
+            tags: ["react", "redux", "axios", "router", "vercel"],
+            githubLink: "https://github.com/Hilal-Bilun-Kara",
+            appLink: "https://github.com/Hilal-Bilun-Kara",
+            image: AreYouBored
+        }
+    ];
 
     return (
         <div className={darkMode ? 'dark' : ''}>
             <section className="w-screen overflow-hidden relative flex flex-col items-center dark:bg-[#484148]">
                 <h1 className='m-[50px] dark:text-[#FFFFFF]'>{t("project")}</h1>
-                
                 <div className="flex">
-                    <div className="w-104 ml-6 mr-20">
-                    <Card body inverse className="text-left mt-10 bg-blue-200 border-blue-200 rounded-lg w-11/12 ml-36 mr-20">
-                            <CardBody>
-                            <CardTitle className="mb-6 font-bold text-2xl text-black font-playfair-display">Random Jokes</CardTitle>
-                                <CardText className='text-[#000000]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.</CardText>
-                                <div className="card-buttons">
-                                    <span className="mt-5 mr-2 inline-block px-8 py-1 bg-white text-black rounded-lg cursor-pointer font-playfair-display">react</span>
-                                    <span className="mt-5 mr-2 inline-block px-8 py-1 bg-white text-black rounded-lg cursor-pointer font-playfair-display">vercel</span>
-                                    <span className="mt-5 mr-2 inline-block px-8 py-1 bg-white text-black rounded-lg cursor-pointer font-playfair-display">axios</span>
-                                    <span className="mt-5 mr-2 inline-block px-8 py-1 bg-white text-black rounded-lg cursor-pointer font-playfair-display">router</span>
-                                </div>
-                                <div className="mt-12">
-                                <a href={`https://github.com/Hilal-Bilun-Kara`} className="text-black mr-36">{t("ViewonGithub")}</a>
-                                <a href={`https://github.com/Hilal-Bilun-Kara`} className="text-black">{t("goToApp")} <span className="font-bold">&rarr;</span></a>
-                                </div>
-                                <CardImg src={RandomJokes} top className="mt-16 w-full" />
-                            </CardBody>
-                        </Card>
-                    </div>
-
-                    <div className="project-card">
-                        <Card body inverse className="text-left mt-10 bg-blue-200 border-blue-200 rounded-lg w-7/12 ml-32 mr-20">
-                            <CardBody>
-                                <CardTitle className="mb-6 font-bold text-2xl text-black font-playfair-display">Are you bored?</CardTitle>
-                                <CardText className="text-black">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia minima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!</CardText>
-                                <div className="card-buttons">
-                                    <span className="mt-5 mr-2 inline-block px-8 py-1 bg-white text-black rounded-lg cursor-pointer font-playfair-display">{t("react")}</span>
-                                    <span className="mt-5 mr-2 inline-block px-8 py-1 bg-white text-black rounded-lg cursor-pointer font-playfair-display">{t("redux")}</span>
-                                    <span className="mt-5 mr-2 inline-block px-8 py-1 bg-white text-black rounded-lg cursor-pointer font-playfair-display">{t("axios")}</span>
-                                    <span className="mt-5 mr-2 inline-block px-8 py-1 bg-white text-black rounded-lg cursor-pointer font-playfair-display">{t("router")}</span>
-                                    <span className="mt-5 mr-2 inline-block px-8 py-1 bg-white text-black rounded-lg cursor-pointer font-playfair-display">{t("vercel")}</span>
-                                </div>
-                                <div className="mt-12">
-                                    <a href={`https://github.com/Hilal-Bilun-Kara`} className="text-black mr-24">{t("ViewonGithub")}</a>
-                                    <a href={`https://github.com/Hilal-Bilun-Kara`} className="text-black">{t("goToApp")} <span className="font-bold">&rarr;</span></a>
-                                </div>
-                                <CardImg src={AreYouBored} top className="mt-8 w-full" />
-                            </CardBody>
-                        </Card>
-                    </div>
+                    {projects.map((project, index) => (
+                        <div key={index} className={`w-104 ml-6 mr-20 ${index === 0 ? "" : "project-card"}`}>
+                            <Card body inverse className="text-left mt-10 bg-blue-200 border-blue-200 rounded-lg w-11/12 ml-36 mr-20">
+                                <CardBody>
+                                    <CardTitle className="mb-6 font-bold text-2xl text-black font-playfair-display">{project.title}</CardTitle>
+                                    <CardText className='text-[#000000]'>{project.description}</CardText>
+                                    <div className="card-buttons">
+                                        {project.tags.map((tag, index) => (
+                                            <span key={index} className="mt-5 mr-2 inline-block px-8 py-1 bg-white text-black rounded-lg cursor-pointer font-playfair-display">{tag}</span>
+                                        ))}
+                                    </div>
+                                    <div className="mt-12">
+                                        <a href={project.githubLink} className="text-black mr-36">{t("ViewonGithub")}</a>
+                                        <a href={project.appLink} className="text-black">{t("goToApp")} <span className="font-bold">&rarr;</span></a>
+                                    </div>
+                                    <CardImg src={project.image} top className="mt-16 w-full" />
+                                </CardBody>
+                            </Card>
+                        </div>
+                    ))}
                 </div>
             </section>
         </div>
-    )
+    );
 }
