1ed27154a1e4067741c770e8043b4b7ddd7333dd
Hilal-Bilun-Kara <bilunkara@gmail.com>
2024-04-20 17:02:22 +0300

    Axios

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,35 @@ import Profile from './components/Profile'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
 import { UserContextProvider } from './context/UserContext'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import i18n from './i18n'
 
 function App () {
   
+  const[allData, setAllData] = useState({})
+  const[loading, setLoading] = useState(true)
 
+
+const fetchData = () => {
+  axios
+  .post("https://reqres.in/api/users", i18n)
+  .then(function (response) {
+    console.log(response);
+    setAllData(response.data);
+    
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+  .finally(() => {
+    setLoading(false);
+  })
+}
+
+useEffect(() => {
+  fetchData();
+}, []);
 
   return (
     
